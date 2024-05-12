e5b8deb409b93dc41bfb03b9198abee484ef423b
TonThong <thongton2310.eht.edu@gmail.com>
2024-05-12 13:22:34 +0700

    4th

--- SourceCode/Main.java
+++ SourceCode/Main.java
@@ -24,68 +24,52 @@ public class Main {
             f.mkdirs();
         }
 
-        ReservationSystem ReservationSystem = new ReservationSystem(accPath, roomPath, roomInAccPath);
-        
-        //add
-        ArrayList<String> req5 = new ArrayList<>();
-        try {
-            Accommodation acc = new Homestay(1, "Cozy Homestay", "123 Main St", "City A", 4.8f);
-            Room room = new Room(1, "Single Room", 1, "Standard", 1, 1, 20.0, 50.0);
-            double totalPrice = ReservationSystem.performReservation("data/reservation_5.csv", acc, room,
-                    new Date(1713368812), new Date(1713398812));
-            req5.add(String.valueOf(totalPrice));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            req5.add(String.valueOf(e.getMessage()));
+        ReservationSystem ReservationSystem = new ReservationSystem(accPath, roomPath, roomInAccPath);  
+
+        for (String arg : args) {
+            switch (arg) {
+                case "1":
+                    // requirement 1
+                    ArrayList<Accommodation> req1 = ReservationSystem.getAccommodations();
+                    writeFile(REQUIREMENT_OUTPUT_FILES[0], req1);
+                    break;
+                case "2":
+                    // requirement 2
+                    List<Accommodation> req2 = ReservationSystem.searchForRoom("City H", 1);
+                    writeFile(REQUIREMENT_OUTPUT_FILES[1], req2);
+                    break;
+                case "3":
+                    // requirement 3
+                    List<Accommodation> req3 = ReservationSystem.searchForRoomByRange("data/reservation_3.csv", 10,
+                            3000,
+                            new Date(1713512750), new Date(1713771950), "City H", 1);
+                    writeFile(REQUIREMENT_OUTPUT_FILES[2], req3);
+                    break;
+                case "4":
+                    // requirement 4
+                    List<Accommodation> req4_1 = ReservationSystem.searchInAdvance("City A", 2, "Standard", null,
+                            null, null, null);
+                    List<Accommodation> req4_2 = ReservationSystem.searchInAdvance("City D", 20, "Standard", true,
+                            5, true, null);
+                    writeFile(REQUIREMENT_OUTPUT_FILES[3], req4_1);
+                    writeFile(REQUIREMENT_OUTPUT_FILES[4], req4_2);
+                    break;
+                case "5":
+                    // requirement 5
+                    ArrayList<String> req5 = new ArrayList<>();
+                    try {
+                        Accommodation acc = new Homestay(1, "Cozy Homestay", "123 Main St", "City A", 4.8f);
+                        Room room = new Room(1, "Single Room", 1, "Standard", 1, 1, 20.0, 50.0);
+                        double totalPrice = ReservationSystem.performReservation("data/reservation_5.csv", acc, room,
+                                new Date(1713368812), new Date(1713398812));
+                        req5.add(String.valueOf(totalPrice));
+                    } catch (Exception e) {
+                        req5.add(String.valueOf(e.getMessage()));
+                    }
+                    writeFile(REQUIREMENT_OUTPUT_FILES[5], req5);
+                    break;
+            }
         }
-        writeFile(REQUIREMENT_OUTPUT_FILES[5], req5);
-
-        
-
-        // for (String arg : args) {
-        //     switch (arg) {
-        //         case "1":
-        //             // requirement 1
-        //             ArrayList<Accommodation> req1 = ReservationSystem.getAccommodations();
-        //             writeFile(REQUIREMENT_OUTPUT_FILES[0], req1);
-        //             break;
-        //         case "2":
-        //             // requirement 2
-        //             List<Accommodation> req2 = ReservationSystem.searchForRoom("City H", 1);
-        //             writeFile(REQUIREMENT_OUTPUT_FILES[1], req2);
-        //             break;
-        //         case "3":
-        //             // requirement 3
-        //             List<Accommodation> req3 = ReservationSystem.searchForRoomByRange("data/reservation_3.csv", 10,
-        //                     3000,
-        //                     new Date(1713512750), new Date(1713771950), "City H", 1);
-        //             writeFile(REQUIREMENT_OUTPUT_FILES[2], req3);
-        //             break;
-        //         case "4":
-        //             // requirement 4
-        //             List<Accommodation> req4_1 = ReservationSystem.searchInAdvance("City A", 2, "Standard", null,
-        //                     null, null, null);
-        //             List<Accommodation> req4_2 = ReservationSystem.searchInAdvance("City D", 20, "Standard", true,
-        //                     5, true, null);
-        //             writeFile(REQUIREMENT_OUTPUT_FILES[3], req4_1);
-        //             writeFile(REQUIREMENT_OUTPUT_FILES[4], req4_2);
-        //             break;
-        //         case "5":
-        //             // requirement 5
-        //             ArrayList<String> req5 = new ArrayList<>();
-        //             try {
-        //                 Accommodation acc = new Homestay(1, "Cozy Homestay", "123 Main St", "City A", 4.8f);
-        //                 Room room = new Room(1, "Single Room", 1, "Standard", 1, 1, 20.0, 50.0);
-        //                 double totalPrice = ReservationSystem.performReservation("data/reservation_5.csv", acc, room,
-        //                         new Date(1713368812), new Date(1713398812));
-        //                 req5.add(String.valueOf(totalPrice));
-        //             } catch (Exception e) {
-        //                 req5.add(String.valueOf(e.getMessage()));
-        //             }
-        //             writeFile(REQUIREMENT_OUTPUT_FILES[5], req5);
-        //             break;
-        //     }
-        // }
     }
 
     public static <E> boolean writeFile(String path, List<E> rows) {
