bbfe1d2a64f6e9550de4efc5f1dba9c244566144
TonThong <thongton2310.eht.edu@gmail.com>
2024-05-12 13:31:28 +0700

    5th

--- CompareFile/ComparePyThon.py
+++ CompareFile/ComparePyThon.py
@@ -1,22 +1,25 @@
+def isTrueOfFile(file):
+    expectedInputPath = "./SourceCode/expected_output/" + file
+    inputPath = "./SourceCode/output/" + file
 
-file = "Req5.txt"
-expectedInputPath = "./SourceCode/expected_output/" + file
-inputPath = "./SourceCode/output/" + file
+    f1 = open(expectedInputPath,'r')
+    f2 = open(inputPath,'r')
 
-f1 = open(expectedInputPath,'r')
-f2 = open(inputPath,'r')
+    while (True):
+        inputLine1 = f1.read().strip()
+        inputLine2 = f2.read().strip()
 
-while (True):
-    inputLine1 = f1.read().strip()
-    inputLine2 = f2.read().strip()
+        if(not inputLine1):
+            print(True)
+            break
+        elif(inputLine1 == inputLine2):
+            continue
+        else: 
+            print(inputLine1)
+            print(inputLine2)        
+            print(False)
+            break
 
-    if(not inputLine1):
-        print(True)
-        break
-    elif(inputLine1 == inputLine2):
-        continue
-    else: 
-        print(inputLine1)
-        print(inputLine2)        
-        print(False)
-        break
+fileList = [ "Req1.txt","Req2.txt","Req3.txt","Req4_1.txt","Req4_2.txt","Req5.txt"]
+for file in fileList:
+    isTrueOfFile(file)
